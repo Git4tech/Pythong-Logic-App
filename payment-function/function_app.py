7e47b29d5bb55f7ca7587219c9a501ed35e72b10
Git4tech <piccolovctr@yahoo.com>
2026-03-25 00:14:00 +0000

    Fix function code

--- payment-function/function_app.py
+++ payment-function/function_app.py
@@ -5,7 +5,7 @@ import logging
 
 app = func.FunctionApp()
 
-@app.route(route="payment_processor", auth_level=func.AuthLevel.ADMIN)
+@app.route(route="payment_processor", auth_level=func.AuthLevel.ANONYMOUS)
 def payment_processor(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
